4b3273c720cc0a0521384a6c9019256663554191
Moreno <mirmoccnt@gmail.com>
2024-03-06 17:35:04 +0100

    Fixed Special tab

--- script.js
+++ script.js
@@ -7,43 +7,17 @@
         });
       });
     });
-  
-  document.addEventListener("DOMContentLoaded", function () {
-    // Ottieni la data attuale
-    var currentDate = new Date();
 
-    // Seleziona tutte le sezioni
-    var sections = document.querySelectorAll("section");
 
-    // Calcola le differenze in millisecondi tra la data attuale e la data di ogni festività
-    var differences = Array.from(sections).map(function (section) {
-      // Estrai il mese e il giorno da ciascuna sezione
-      var sectionDate = new Date(currentDate.getFullYear(), parseInt(section.dataset.date.substring(0, 2)) - 1, parseInt(section.dataset.date.substring(3, 5)));
-
-      // Calcola la differenza in millisecondi tra le date
-      var difference = sectionDate - currentDate;
-
-      // Considera solo le differenze positive
-      return difference > 0 ? difference : Infinity;
-    });
-
-    // Combina le sezioni con le rispettive differenze
-    var sectionsWithDifferences = Array.from(sections).map(function (section, index) {
-      return { section: section, difference: differences[index] };
-    });
-
-    // Ordina le sezioni in base alla differenza in millisecondi
-    var sortedSections = sectionsWithDifferences.sort(function (a, b) {
-      return a.difference - b.difference;
-    }).map(function (item) {
-      return item.section;
-    });
-
-    // Seleziona il contenitore delle sezioni
-    var container = document.querySelector(".container");
-
-    // Sostituisci le sezioni nell'ordine ordinato
-    sortedSections.forEach(function (section) {
-      container.appendChild(section);
-    });
-  });
+    function navigateToSection() {
+        const selectElement = document.getElementById("inputGroupSelect01");
+        const selectedValue = selectElement.value;
+    
+        if (selectedValue !== "") {
+          const section = document.getElementById(selectedValue);
+          if (section) {
+            section.scrollIntoView({ behavior: "smooth" });
+          }
+        }
+      }
+    
