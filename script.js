7710edcd8cfc629a6715faba3c7874536cf9cf12
Moreno <mirmoccnt@gmail.com>
2024-03-12 15:01:28 +0100

    Better SEO and performance

--- script.js
+++ script.js
@@ -9,44 +9,6 @@
     });
     
 
-    document.addEventListener("DOMContentLoaded", function () {
-      // Verifica se il cookie di accettazione è presente
-      const cookieAccepted = localStorage.getItem("cookieAccepted");
-    
-      // Se il cookie non è stato accettato, mostra il banner
-      if (!cookieAccepted) {
-        const cookieBanner = document.createElement("div");
-        cookieBanner.innerHTML = `
-          <div class="cookie-banner text-white text-center py-2 bg-dark fixed-bottom">
-            Questo sito utilizza i cookie per garantire una migliore esperienza di navigazione. 
-            <button class="btn btn-success btn-sm ms-2" id="acceptCookie">Accetta</button>
-            <button class="btn btn-danger btn-sm ms-2" id="rejectCookie">Rifiuta</button>
-          </div>
-        `;
-    
-        // Aggiungi il banner al corpo del documento
-        document.body.appendChild(cookieBanner);
-    
-        // Aggiungi un listener al pulsante di accettazione dei cookie
-        document.getElementById("acceptCookie").addEventListener("click", function () {
-          // Nascondi il banner
-          cookieBanner.style.display = "none";
-    
-          // Imposta il cookie di accettazione
-          localStorage.setItem("cookieAccepted", true);
-        });
-    
-        // Aggiungi un listener al pulsante di rifiuto dei cookie
-        document.getElementById("rejectCookie").addEventListener("click", function () {
-          // Nascondi il banner
-          cookieBanner.style.display = "none";
-    
-          // Puoi fare ulteriori azioni qui se necessario
-        });
-      }
-    });
-    
-
     function navigateToSection() {
         const selectElement = document.getElementById("inputGroupSelect01");
         const selectedValue = selectElement.value;
@@ -91,31 +53,9 @@
           previewModal.hide();
         });
       });
-      
-
-    // Funzione per chiudere il popup
-    function closePopup() {
-      document.getElementById('popup').style.display = 'none';
-      document.getElementById('overlay').style.display = 'none';
-  }
-
-  // Verifica se è la prima visita utilizzando i cookie
-  function isFirstVisit() {
-      return document.cookie.indexOf('visited=true') === -1;
-  }
-
-  // Mostra il popup solo se è la prima visita
-  if (isFirstVisit()) {
-      document.getElementById('popup').style.display = 'block';
-      document.getElementById('overlay').style.display = 'block';
-
-      // Imposta un cookie per indicare che il sito è stato visitato
-      document.cookie = 'visited=true; expires=Fri, 31 Dec 9999 23:59:59 GMT; path=/';
-  }
 
   // Al momento, la nuvoletta non fa nulla quando viene cliccata
   document.getElementById('chatBotContainer').addEventListener('click', function() {
-    // Azioni da eseguire quando la nuvoletta viene cliccata
-    // Per ora, non fa nulla
+
 });
 
